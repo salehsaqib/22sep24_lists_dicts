7b6b914232c224af9aeb44e7725b8ce55df414b4
Muhammad Saleh Saqib <salehsaqib@gmail.com>
2024-09-24 20:44:04 +0500

    dict_average updated

--- dict_average.py
+++ dict_average.py
@@ -3,13 +3,16 @@
 # and the values are lists of marks. Add marks for 3 subjects, and print the average marks for each subject.
 
 average_marks = {
-    "English": 90,
-    "Urdu": 75,
-    "Math": 100    
+    "English": [78,55,12,40,59,95,53,78],
+    "Urdu": [78,23,45,96,75,45],
+    "Math": [84,58,76,23,90,100]
 }
-sum:int = 0
-count:int = 0
 for x in average_marks:
-    sum += int(average_marks[x])
-    count +=1
-print(f"The Average Marks of {count} Subjects =  {int(sum/count)}")
+    print(f"Average Marks of {x} : ",end=" ")
+    sum:int = 0
+    count:int = 0
+    for y in average_marks[x]:
+        sum += y
+        count +=1
+    print(int(round((sum/count),0)))
+
